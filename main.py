efeeed9d57616f3c210435eebaaafdcd4058aeb6
Weronika <weronika.piotrowska.pl@gmail.com>
2023-01-02 19:16:20 +0100

    Added hParams

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from tensorboard.plugins.hparams import api as hp
 
 
 def generate_datasets():
+    base_dir = 'dataset'
     train_dataset = tf.keras.utils.image_dataset_from_directory(base_dir, image_size=(200, 200), subset='training',
                                                                 seed=1,
                                                                 validation_split=0.1, batch_size=32)
@@ -39,12 +40,12 @@ def build_model(hparams):
         layers.MaxPooling2D(2, 2),
 
         layers.Flatten(),
-        layers.Dense(512, activation='relu'),
+        layers.Dense(hparams[HP_NUM_UNITS], activation='relu'),
         layers.BatchNormalization(),
-        layers.Dense(512, activation='relu'),
+        layers.Dense(hparams[HP_NUM_UNITS], activation='relu'),
         layers.Dropout(hparams[HP_DROPOUT]),
         layers.BatchNormalization(),
-        layers.Dense(512, activation='relu'),
+        layers.Dense(hparams[HP_NUM_UNITS], activation='relu'),
         layers.Dropout(hparams[HP_DROPOUT]),
         layers.BatchNormalization(),
         layers.Dense(1, activation='sigmoid')
@@ -52,28 +53,32 @@ def build_model(hparams):
 
 
 def run_training(train_dataset, test_dataset, hparams):
+    log_dir = "logs/fit/" + "dropout=" + str(hparams[HP_DROPOUT]) + "num_units" + str(hparams[HP_NUM_UNITS])
+    with tf.summary.create_file_writer(log_dir).as_default():
+        hp.hparams(hparams)
     model = build_model(hparams)
     model.summary()
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    log_dir = "logs/fit/" + "dropout=" + str(hparams[HP_DROPOUT])
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
     model_checkpoint_callback = generate_checkpoint_callback(hparams)
-    return model.fit(train_dataset, epochs=10, validation_data=test_dataset,
+    return model.fit(train_dataset, epochs=5, validation_data=test_dataset,
                      callbacks=[tensorboard_callback, model_checkpoint_callback])
 
 
-base_dir = 'dataset'
 train_dataset, test_dataset = generate_datasets()
 HP_DROPOUT = hp.HParam('dropout', hp.Discrete([0.1, 0.2]))
+HP_NUM_UNITS = hp.HParam('num_units', hp.Discrete([512, 1024]))
 with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
-    hp.hparams_config(hparams=[HP_DROPOUT], metrics=[hp.Metric("accuracy", display_name='Accuracy')])
+    hp.hparams_config(hparams=[HP_NUM_UNITS, HP_DROPOUT], metrics=[hp.Metric("accuracy", display_name='Accuracy')])
 
 for dropout_rate in HP_DROPOUT.domain.values:
-    hparams = {
-        HP_DROPOUT: dropout_rate
-    }
-    history = run_training(train_dataset, test_dataset, hparams)
-    history_df = pd.DataFrame(history.history)
-    history_df.loc[:, ['loss', 'val_loss']].plot()
-    history_df.loc[:, ['accuracy', 'val_accuracy']].plot()
-    plt.show()
+    for num_units in HP_NUM_UNITS.domain.values:
+        hparams = {
+            HP_DROPOUT: dropout_rate,
+            HP_NUM_UNITS: num_units
+        }
+        history = run_training(train_dataset, test_dataset, hparams)
+        history_df = pd.DataFrame(history.history)
+        history_df.loc[:, ['loss', 'val_loss']].plot()
+        history_df.loc[:, ['accuracy', 'val_accuracy']].plot()
+        plt.show()
